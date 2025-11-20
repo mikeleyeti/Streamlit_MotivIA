82cb3ff358322e41f0f70e85113fdfbc8275eaac
mikeleyeti <mikeleyeti@yahoo.fr>
2025-11-20 09:41:58 +0100

    Essai data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Streamlit_MotivIA
 Streamlit MotivIA
+
+[Lien vers l'application en ligne](https://appmotivia.streamlit.app/)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+
+# import numpy as np
 
 st.title("Analyse des questionnaires MotivIA")
 st.subheader("Données de l'Académie d'Orléans-Tours.")
+
+df_prof = pd.read_csv("Data/profs.csv", index_col=0)
+st.dataframe(df_prof)
